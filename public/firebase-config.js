c776ab03326b1bdd6ee11864f33f16ee408d2752
Dinesh Ramakrishnan <69108453+dineshmc1@users.noreply.github.com>
2025-11-30 14:27:57 +0800

    Clear Firebase config values for security
    Removed sensitive Firebase configuration details.

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,8 +1,8 @@
 window.__firebase_config = JSON.stringify({
-  apiKey: "AIzaSyApO9nn69YG2_AM54pYEwMvnUeFAijHaXE",
-  authDomain: "kanban-board-61bd4.firebaseapp.com",
-  projectId: "kanban-board-61bd4",
-  storageBucket: "kanban-board-61bd4.firebasestorage.app",
-  messagingSenderId: "159651810522",
-  appId: "1:159651810522:web:f0dfee87727ea4d954a9e3",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 });
